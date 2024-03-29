df33d04f6b9ae4d491cfbd186a16faf36d320820
claudioDsi <claudio.disipio94@gmail.com>
2024-03-29 09:32:51 -0400

    changed rules

--- KB_builder/visualization.py
+++ KB_builder/visualization.py
@@ -2,44 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from matplotlib.ticker import MaxNLocator
 
-def plot_d1_stats():
+def plot_d1_stats(figsize=(4, 4)):
     # Data
-    metamodels = ['Families', 'IEEE', 'MySQL', 'Table', 'XML']
-    classes =       [2, 14, 8, 3,5]
-    attributes =    [2, 2, 7, 1,6]
-    relationships = [8, 31, 7, 2,2]
+    domains = ['Families', 'MySQL', 'IEEE1471', 'Table']
+    create =  [8, 2, 7, 3]
+    edit = [0, 0, 0, 0]
+    remove = [0, 0, 0, 0]
 
-    x = np.arange(len(metamodels))  # the label locations
+    x = np.arange(len(domains))  # the label locations
     width = 0.2  # the width of the bars
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=figsize)
     # Using different shades of blue for each category
-    rects1 = ax.bar(x - width, classes, width, label='Classes', color='peachpuff')  # Light Orange
-    rects2 = ax.bar(x, attributes, width, label='Attributes', color='sandybrown')  # Medium Orange
-    rects3 = ax.bar(x + width, relationships, width, label='Relationships', color='chocolate')  # Dark Orange
+    rects1 = ax.bar(x - width, create, width, label='Create', color='darkred')  # Light Orange
+    rects2 = ax.bar(x, edit, width, label='Edit', color='sandybrown')  # Medium Orange
+    rects3 = ax.bar(x + width, remove, width, label='Remove', color='chocolate')  # Dark Orange
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Counts')
     ax.set_title('')
     ax.set_xticks(x)
-    ax.set_xticklabels(metamodels)
+    ax.set_xticklabels(domains)
     ax.legend()
 
-    def autolabel(rects):
-        """Attach a text label above each bar in *rects*, displaying its height."""
-        for rect in rects:
-            height = rect.get_height()
-            ax.annotate('{}'.format(height),
-                        xy=(rect.get_x() + rect.get_width() / 2, height),
-                        xytext=(0, 3),  # 3 points vertical offset
-                        textcoords="offset points",
-                        ha='center', va='bottom')
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
-    autolabel(rects1)
-    autolabel(rects2)
-    autolabel(rects3)
 
-    fig.tight_layout()
+    #fig.tight_layout()
 
     plt.show()
     plt.savefig('d1_stats.pdf')
@@ -82,4 +72,4 @@ def plot():
     plt.show()
 
 if __name__ == "__main__":
-    plot()
+    plot_d1_stats()
